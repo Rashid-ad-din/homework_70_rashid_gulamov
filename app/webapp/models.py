4836c58d3f307c2ecfc1910965dc4e0479c4a50d
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-09-18 20:49:26 +0600

    Коммит создания модели базы данных. Был осуществлен перенакат, > т.к. в первый раз создалась некорректная модель. При этом запись создания первой модели осталась в истории  migrations (0001_initial.py).

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    description = models.CharField(max_length=250, null=False, blank=False, verbose_name='Описание')
+    state = models.CharField(max_length=50, null=False, blank=False, default='new', verbose_name='Статус')
+    date_to_do = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return f'Описание: {self.description}, Статус: {self.state}, Дата выполнения: {self.date_to_do}'

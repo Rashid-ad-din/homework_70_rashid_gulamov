20f1e0fc955d78464230b3a0d834cbd4e67b18e7
userRashid Gulamov <rashidgulamov7@gmail.com>
2022-09-20 03:32:52 +0600

    В поле статуса модели добавлен choice. Также откорректированы параметры полей в соответствии с условями задачи.

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 
 class Item(models.Model):
+    new = 'Новая'
+    process = 'В процессе'
+    done = 'Сделано'
+    CHOICES = [
+        ('new', 'Новая'),
+        ('process', 'В процессе'),
+        ('done', 'Сделано')
+    ]
     description = models.CharField(max_length=250, null=False, blank=False, verbose_name='Описание')
-    state = models.CharField(max_length=50, null=False, blank=False, default='new', verbose_name='Статус')
-    date_to_do = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+    state = models.CharField(max_length=50, null=False, blank=False, choices=CHOICES, default=new,
+                             verbose_name='Статус')
+    date_to_do = models.DateField(null=True, blank=False, verbose_name='Дата выполнения')
 
     def __str__(self):
-        return f'Описание: {self.description}, Статус: {self.state}, Дата выполнения: {self.date_to_do}'
+        return f'Описание: {self.description}, Статус: {self.get_state_display()}, Дата выполнения: {self.date_to_do}'

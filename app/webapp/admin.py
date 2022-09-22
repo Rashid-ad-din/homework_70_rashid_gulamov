eebd87c2ed56e152a58841bdc6c38405014ecc01
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-09-23 03:13:51 +0600

    Произведена корректировка кода в админке в целях корректного отображения информации

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 from webapp.models import Item
 
-list_display = ('id', 'state', 'desscription', 'date_to_do')
-list_filter = ('id', 'state', 'desscription', 'date_to_do')
-search_fields = ('state', 'description', 'date_to_do')
-fields = ('id', 'state', 'desscription', 'date_to_do')
-readonly_fields = ('id', 'date_to_do')
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'state', 'description', 'date_to_do')
+    list_filter = ('id', 'state', 'description', 'date_to_do')
+    search_fields = ('state', 'description', 'date_to_do')
+    fields = ('id', 'state', 'description', 'description_details', 'date_to_do')
+    readonly_fields = ('id', 'date_to_do')
+
+
+admin.site.register(Item, ItemAdmin)

7ce22c6eff168fef5325fbadd16a1cd39725ecf5
userRashid Gulamov <rashidgulamov7@gmail.com>
2022-09-22 22:27:04 +0600

    Добавлен код для представления таблицы в админке.

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from webapp.models import Item
 
+list_display = ('id', 'state', 'desscription', 'date_to_do')
+list_filter = ('id', 'state', 'desscription', 'date_to_do')
+search_fields = ('state', 'description', 'date_to_do')
+fields = ('id', 'state', 'desscription', 'date_to_do')
+readonly_fields = ('id', 'date_to_do')
+
 admin.site.register(Item)

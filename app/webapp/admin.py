9fef72a758664141166b9f18d232e8bcb868e9f7
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-10-06 23:39:03 +0600

    Модифицирована админка, созданы методы для отображения полей со связанных таблиц, создано 5 задач, произведен дамп фикстуры

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from app.webapp.models import Item, Task, Type, State
+from webapp.models import Item, Task, Type, State
 
 
 class ItemAdmin(admin.ModelAdmin):
@@ -12,11 +12,17 @@ class ItemAdmin(admin.ModelAdmin):
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'summary', 'description', 'state', 'type', 'created_at', 'updated_at')
+    list_display = ('id', 'summary', 'description', 'get_state', 'get_type', 'created_at', 'updated_at')
     list_filter = ('id', 'summary', 'state', 'type')
     search_fields = ('summary', 'state', 'type')
     fields = ('id', 'summary', 'description', 'state', 'type', 'created_at', 'updated_at')
     readonly_fields = ('id', 'created_at', 'updated_at')
+
+    def get_state(self, obj):
+        return "\n".join([s.name for s in obj.state.all()])
+
+    def get_type(self, obj):
+        return "\n".join([t.name for t in obj.type.all()])
 
 
 class StateAdmin(admin.ModelAdmin):

96428dd5e696611244a27ce6a523b83b32b9bf2c
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-10-06 23:03:58 +0600

    Созданные модели задач, статусов, типов зарегистрированы в админке

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from webapp.models import Item
+from app.webapp.models import Item, Task, Type, State
 
 
 class ItemAdmin(admin.ModelAdmin):
@@ -11,4 +11,31 @@ class ItemAdmin(admin.ModelAdmin):
     readonly_fields = ('id', 'date_to_do')
 
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'summary', 'description', 'state', 'type', 'created_at', 'updated_at')
+    list_filter = ('id', 'summary', 'state', 'type')
+    search_fields = ('summary', 'state', 'type')
+    fields = ('id', 'summary', 'description', 'state', 'type', 'created_at', 'updated_at')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('id', 'name')
+    search_fields = ('id', 'name')
+    fields = ('id', 'name')
+    readonly_fields = ('id',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('id', 'name')
+    search_fields = ('id', 'name')
+    fields = ('id', 'name')
+    readonly_fields = ('id',)
+
+
 admin.site.register(Item, ItemAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(Type, TypeAdmin)

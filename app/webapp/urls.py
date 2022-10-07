2934b675421a657d4925e3276058158d72d73356
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-10-07 12:00:02 +0600

    Добавлена операция редактирования задачи с соответствующими шаблоном и вьюшкой.

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 from webapp.views.base import HomeView
 from webapp.views.items import item_view, add_view, edit_view, delete_view, items_view, confirm_delete_view
-from webapp.views.tasks import TasksView, TaskView, AddTaskView
+from webapp.views.tasks import TasksView, TaskView, AddTaskView, UpdateView
 
 urlpatterns = [
     path('', TasksView.as_view(), name='show_tasks'),
     path('items/add/', AddTaskView.as_view(), name='add_task'),
     path('items/', TasksView.as_view(), name='show_tasks'),
     path('items/<pk>/', TaskView.as_view(), name='show_task'),
-    path('items/<pk>/edit/', edit_view, name='edit_item'),
+    path('items/<pk>/edit/', UpdateView.as_view(), name='edit_task'),
     path('items/<pk>/delete/', delete_view, name='delete_item'),
     path('items/<pk>/confirm-delete/', confirm_delete_view, name='confirm_delete_item')
 ]
--- app/webapp/views/tasks.py
+++ app/webapp/views/tasks.py
@@ -41,3 +41,26 @@ class TaskView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
         return context
+
+
+class UpdateView(TemplateView):
+    template_name = 'edit_task.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = TaskForm(instance=context['task'])
+        context['form'] = form
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs.get('pk'))
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return redirect('show_task', pk=task.pk)
+        return render(request, 'edit_task.html', context={'form': form})

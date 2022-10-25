e3d22f492383ed781248d98d22b60ebb395686e1
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-10-25 14:05:08 +0600

    Добавлен функционал регистрации пользователей. При переключении между страницами регистрации и войти параметр next не оторбражается в адресной строке

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.forms import TextInput, PasswordInput, EmailInput
 
 
 class LoginForm(forms.Form):
@@ -7,3 +10,43 @@ class LoginForm(forms.Form):
     password = forms.CharField(required=True, label='Пароль',
                                widget=forms.PasswordInput(attrs={'class': "form-control w-75"}))
     next = forms.CharField(required=False, widget=forms.HiddenInput)
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Подтвердите пароль', strip=False, required=True,
+                                       widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password_confirm', 'first_name', 'last_name', 'email')
+        widgets = {
+            'username': TextInput(attrs={'class': "form-control w-75"}),
+            'password': PasswordInput(attrs={'class': "form-control w-75"}),
+            'password_confirm': PasswordInput(attrs={'class': "form-control w-75"}),
+            'first_name': TextInput(attrs={'class': "form-control w-75"}),
+            'last_name': TextInput(attrs={'class': "form-control w-75"}),
+            'email': EmailInput(attrs={'class': "form-control w-75"}),
+
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
+        first_name = cleaned_data.get('first_name')
+        if not first_name:
+            raise ValidationError('Поле "Имя" обязательно к заполнению')
+        email = cleaned_data.get('email')
+        if not email:
+            raise ValidationError('Поле "email" обязательно к заполнению')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        # user.groups.add('user')
+        if commit:
+            user.save()
+        return user
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth import authenticate, login, logout
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.urls import reverse
+from django.views.generic import TemplateView, CreateView
 
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, CustomUserCreationForm
 
 
 class LoginView(TemplateView):
     template_name = 'login.html'
     form = LoginForm
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: WSGIRequest, *args, **kwargs):
         next = request.GET.get('next')
         form_data = {} if not next else {'next': next}
         form = self.form(form_data)
@@ -35,3 +37,26 @@ class LoginView(TemplateView):
 def logout_view(request):
     logout(request)
     return redirect('show_tasks')
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.get_success_url())
+        context = {}
+        context['form'] = form
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('tasks')
+        return next_url

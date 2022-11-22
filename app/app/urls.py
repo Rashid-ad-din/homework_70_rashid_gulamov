09a76653e7a2dec11ae212cf6efd2c058ccea5b3
Rashid Gulamov <rashidgulamov7@gmail.com>
2022-11-22 11:30:38 +0600

    Создание API путей для методов просмотра, изменения и удаления (get, put, delete)

--- app/app/urls.py
+++ app/app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
     path('auth/', include('accounts.urls')),
+    path('api/', include('api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
